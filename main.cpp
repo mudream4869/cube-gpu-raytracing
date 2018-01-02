4c880251afc068dd8e618590dfdde4052800f250
Mudream <mudream4869@gmail.com>
2018-01-02 19:29:45 +0800

    add different cube

--- main.cpp
+++ main.cpp
@@ -13,30 +13,19 @@
 #include "basic.h"
 #include "sampler.h"
 
-struct Sphere {
-    Vec3d c;
-    double r;
-    Sphere(const Vec3d& c, double r) : c(c), r(r) {}
-    Vec3d getNormal(const Vec3d& pi) const { return (pi - c) / r; }
-    bool intersect(const Ray& ray, double &t) const {
-        const Vec3d o = ray.o;
-        const Vec3d d = ray.d;
-        const Vec3d oc = o - c;
-        const double b = 2 * dot(oc, d);
-        const double c = dot(oc, oc) - r*r;
-        double disc = b*b - 4 * c;
-        if (disc < 1e-4) return false;
-        disc = sqrt(disc);
-        const double t0 = -b - disc;
-        const double t1 = -b + disc;
-        t = (t0 < t1) ? t0 : t1;
-        return true;
-    }
-};
-
 const int MAX_X = 50, MAX_Y = 50, MAX_Z = 50;
 const double cubeWidth = 50; // Cube size
 int Cubes[MAX_X][MAX_Y][MAX_Z] = {0};
+
+const int CUBE_NULL  = 0;
+const int CUBE_WATER = 1;
+const int CUBE_GRASS = 2;
+const int CUBE_MUD   = 3;
+const int CUBE_LIGHT = 4;
+
+int& getCube(const Vec3i& i){
+    return Cubes[i.x][i.y][i.z];
+}
 
 // Return True if pos in [0, MAX_X)*[0, MAX_Y)*[0, MAX_Z)
 bool isOutOfWorld(const Vec3d& pos){
@@ -68,7 +57,7 @@ bool intersectCubes(const Ray& ray, Vec3i& index, double& now_t){
             d_y = floor(curr.y/width),
             d_z = floor(curr.z/width);
         
-        if(Cubes[d_x][d_y][d_z]){
+        if(Cubes[d_x][d_y][d_z] != CUBE_NULL){
             index = Vec3i(d_x, d_y, d_z);
             return true;
         }
@@ -106,7 +95,6 @@ Vec3d getNormal(const Ray& ray, const Vec3i& index, double t){
     if(az >= std::max(ax, ay)) return Vec3d(0, 0, r.z > 0 ? 1 : -1);
     
     assert(0);
-           
 }
 
 Ray reflect(const Ray& ray, const Vec3i& cubeIndex, double t){
@@ -125,26 +113,56 @@ Ray reflect(const Ray& ray, const Vec3i& cubeIndex, double t){
     return Ray(ray(t), N*dt*2 + ray.d, 0.01);
 }
 
+Ray patrenate(const Ray& ray, const Vec3i& cubeIndex, double t){
+    Vec3d curr = ray(t);
+
+    int d_x = floor(curr.x/cubeWidth),
+        d_y = floor(curr.y/cubeWidth),
+        d_z = floor(curr.z/cubeWidth);
+
+    double n_x = d_x*cubeWidth, n_y = d_y*cubeWidth, n_z = d_z*cubeWidth;
+
+    if(ray.d.x > 0) n_x += cubeWidth;
+    if(ray.d.y > 0) n_y += cubeWidth;
+    if(ray.d.z > 0) n_z += cubeWidth;
+
+    double min_t = std::numeric_limits<double>::infinity();
+
+    if(not isZero(ray.d.x)) min_t = std::min((n_x - ray.o.x)/ray.d.x, min_t);
+    if(not isZero(ray.d.y)) min_t = std::min((n_y - ray.o.y)/ray.d.y, min_t);
+    if(not isZero(ray.d.z)) min_t = std::min((n_z - ray.o.z)/ray.d.z, min_t);
+
+    return Ray(ray.o, ray.d, min_t + 0.01); 
+}
+
 Vec3d rayTracing(const Ray& ray, int level = 0){
     const Vec3d white(1, 1, 1);
     const Vec3d black(0, 0, 0);
     const Vec3d red(1, 0, 0);
+    const Vec3d green(0, 1, 0);
+    const Vec3d brown(139/255.,69/255.,19/255.);
 
     if(level >= 10){
         return black;
     }
 
-    const Sphere light1(Vec3d(1800, 0, 1000), 500);
-
     Vec3i cubeIndex;
     double tt;
 
     if(intersectCubes(ray, cubeIndex, tt)){
-        return rayTracing(reflect(ray, cubeIndex, tt), level+1)*red;
-    }
+        int cube_id = getCube(cubeIndex);
+        
+        if(cube_id == CUBE_LIGHT){
+            if(level)
+                return white;
+            else
+                return rayTracing(patrenate(ray, cubeIndex, tt), level);
+        }
 
-    if(light1.intersect(ray, tt)){
-        return white;
+        Vec3d cube_color;
+        if(cube_id == CUBE_MUD)   cube_color = brown;
+        if(cube_id == CUBE_GRASS) cube_color = green;
+        return rayTracing(reflect(ray, cubeIndex, tt), level+1)*cube_color;
     }
 
     return black;
@@ -156,9 +174,11 @@ int main(){
 
     for(int lx = 0;lx < MAX_X;lx++)
         for(int lz = 0;lz < MAX_Z;lz++){
-            Cubes[lx][9][lz] = 1;
-            if(lx&1 and lz&1)
-                Cubes[lx][8][lz] = 1;
+            Cubes[lx][9][lz] = CUBE_MUD;
+            if(lx%2 == 1 and lz%2 == 1)
+                Cubes[lx][8][lz] = CUBE_GRASS;
+            if(lx%4 == 0 and lz%4 == 0)
+                Cubes[lx][8][lz] = CUBE_LIGHT;
         }
 
     double t;
@@ -175,7 +195,7 @@ int main(){
 
                 int x = _x*4 + id;
 
-                const int SAMPLE_COUNT = 16;
+                const int SAMPLE_COUNT = 128;
 
                 double sx[SAMPLE_COUNT], sy[SAMPLE_COUNT];
                 samplesSquare(sx, sy, SAMPLE_COUNT);
@@ -188,10 +208,7 @@ int main(){
                     col = col + rayTracing(ray);
                 }
 
-                col = col/SAMPLE_COUNT;
-                col = col*255;
-                
-                pic[y][x] = col;
+                pic[y][x] = col/SAMPLE_COUNT;
             }
         }
     };
@@ -212,6 +229,7 @@ int main(){
     for(int ly = 0;ly < H;ly++){
         for(int lx = 0;lx < W;lx++){
             auto& col = pic[ly][lx];
+            col = col*255;
             out << (int)col.x << ' '
                 << (int)col.y << ' '
                 << (int)col.z << '\n';
